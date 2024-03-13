78ae4051ed31f214679f2745de212dfae084bdbe
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-14 00:49:50 +0800

    Update BackToTop.js

--- BackToTop.js
+++ BackToTop.js
@@ -1,7 +1,6 @@
 function scrollToTop() {
         document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-      }
+        document.documentElement.scrollTop = 0; 
     
       window.onscroll = function() {
       const scrollToTopButton = document.getElementById('scrollToTop');
