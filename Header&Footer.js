2d2e715537a37d57f118ca4f18547d64fd99f9d2
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-14 01:07:57 +0800

    Update Header&Footer.js

--- Header&Footer.js
+++ Header&Footer.js
@@ -6,10 +6,3 @@ document.addEventListener('DOMContentLoaded', function() {
        });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-            document.getElementById('footer').innerHTML = data;
-       });
-});
