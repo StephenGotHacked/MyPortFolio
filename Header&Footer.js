1340283a0acc574c12c682de36658cae06baf725
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-14 00:39:38 +0800

    Update Header&Footer.js

--- Header&Footer.js
+++ Header&Footer.js
@@ -13,3 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('footer').innerHTML = data;
        });
 });
+
+function scrollToTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+      }
+    
+      window.onscroll = function() {
+      const scrollToTopButton = document.getElementById('scrollToTop');
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+             scrollToTopButton.style.display = 'block';
+          }else {
+          scrollToTopButton.style.display = 'none';
+          }
+      };
