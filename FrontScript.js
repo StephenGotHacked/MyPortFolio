d289df70f03d57d32ce8bc6b6ca6d0eed484d06a
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-15 02:55:14 +0800

    Update FrontScript.js

--- FrontScript.js
+++ FrontScript.js
@@ -13,3 +13,31 @@ function getRandomColor() {
   }
 
   setInterval(changeColor, 2000);
+document.addEventListener("DOMContentLoaded", function() {
+    var text = "Hello Tech-nerds, I'm Stephen David Q. Condino";
+    var container = document.getElementById("name");
+    var delay = 50;
+         
+    for (var i = 0; i < text.length; i++) {
+        (function(i) {
+        setTimeout(function() {
+        container.textContent += text[i];
+    }, i * delay);
+    })(i);
+    }
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var text = "The Author of this Website";
+    var container = document.getElementById("author");
+    var delay = 100;
+          
+    for (var i = 0; i < text.length; i++) {
+        (function(i) {
+        setTimeout(function() {
+        container.textContent += text[i];
+    }, i * delay);
+    })(i);
+    }
+    });
+
