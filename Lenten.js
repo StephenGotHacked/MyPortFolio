4472c90d93eba6ad0a786bd4a1603a0626a4e07d
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-29 01:02:12 +0800

    Create Lenten.js

---
+++ Lenten.js
@@ -0,0 +1,23 @@
+const tableData = document.querySelectorAll('.tabdata');
+const paragraphs = document.querySelectorAll('#means p');
+
+  tableData.forEach((element) => {
+    element.addEventListener('click', function() {
+      const paragraphNumber = this.getAttribute('data-paragraph');
+      
+      // Hide all paragraphs
+      paragraphs.forEach((p) => {
+        p.classList.add('hiddenmess');
+      });
+      
+      // Show the selected paragraph
+      document.getElementById('paragraph' + paragraphNumber).classList.remove('hiddenmess');
+
+      tableData.forEach(td => {
+        td.classList.remove('selected');
+      });
+      
+      // Add the 'selected' class to the clicked table data element
+      this.classList.add('selected');
+    });
+  });
