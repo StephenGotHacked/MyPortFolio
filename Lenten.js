a420950c7c599cdcb7896f2276e91be7d9526722
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-30 21:21:20 +0800

    Update Lenten.js

--- Lenten.js
+++ Lenten.js
@@ -1,23 +1,25 @@
-const tableData = document.querySelectorAll('.tabdata');
-const paragraphs = document.querySelectorAll('#means div');
+ const tableData = document.querySelectorAll('.tabdata');
+            const paragraphs = document.querySelectorAll('#means div');
 
-  tableData.forEach((element) => {
-    element.addEventListener('click', function() {
-      const paragraphNumber = this.getAttribute('data-paragraph');
-      
-      // Hide all paragraphs
-      paragraphs.forEach((p) => {
-        p.classList.add('hiddenmess');
-      });
-      
-      // Show the selected paragraph
-      document.getElementById('paragraph' + paragraphNumber).classList.remove('hiddenmess');
+            tableData.forEach((element) => {
+              element.addEventListener('click', function() {
+                const paragraphNumber = this.getAttribute('data-paragraph');
 
-      tableData.forEach(td => {
-        td.classList.remove('selected');
-      });
-      
-      // Add the 'selected' class to the clicked table data element
-      this.classList.add('selected');
-    });
-  });
+                paragraphs.forEach((p) => {
+                  p.classList.add('hiddenmess');
+                });
+
+                const selectedParagraph = document.getElementById('paragraph' + paragraphNumber);
+                selectedParagraph.classList.remove('hiddenmess');
+
+                void selectedParagraph.offsetWidth;
+
+                selectedParagraph.style.opacity = '1';
+
+                tableData.forEach(td => {
+                  td.classList.remove('selected');
+                });
+
+                this.classList.add('selected');
+              });
+            });
