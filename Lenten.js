5c7556e32f9db15ca0555b4161db5f782ece53b8
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-03-31 15:58:39 +0800

    Update Lenten.js

--- Lenten.js
+++ Lenten.js
@@ -1,25 +1,48 @@
- const tableData = document.querySelectorAll('.tabdata');
-            const paragraphs = document.querySelectorAll('#means div');
+            let nextDom = document.getElementById('next');
+            let prevDom = document.getElementById('prev');
 
-            tableData.forEach((element) => {
-              element.addEventListener('click', function() {
-                const paragraphNumber = this.getAttribute('data-paragraph');
+            let carouselDom = document.querySelector('.carousel');
+            let SliderDom = carouselDom.querySelector('.carousel .list');
+            let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+            let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+            let timeDom = document.querySelector('.carousel .time');
 
-                paragraphs.forEach((p) => {
-                  p.classList.add('hiddenmess');
-                });
+            thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+            let timeRunning = 3000;
+            let timeAutoNext = 7000;
 
-                const selectedParagraph = document.getElementById('paragraph' + paragraphNumber);
-                selectedParagraph.classList.remove('hiddenmess');
+            nextDom.onclick = function(){
+                showSlider('next');    
+            }
 
-                void selectedParagraph.offsetWidth;
+            prevDom.onclick = function(){
+                showSlider('prev');    
+            }
+            let runTimeOut;
+            let runNextAuto = setTimeout(() => {
+                next.click();
+            }, timeAutoNext)
+            function showSlider(type){
+                let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+                let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+                
+                if(type === 'next'){
+                    SliderDom.appendChild(SliderItemsDom[0]);
+                    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+                    carouselDom.classList.add('next');
+                }else{
+                    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+                    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+                    carouselDom.classList.add('prev');
+                }
+                clearTimeout(runTimeOut);
+                runTimeOut = setTimeout(() => {
+                    carouselDom.classList.remove('next');
+                    carouselDom.classList.remove('prev');
+                }, timeRunning);
 
-                selectedParagraph.style.opacity = '1';
-
-                tableData.forEach(td => {
-                  td.classList.remove('selected');
-                });
-
-                this.classList.add('selected');
-              });
-            });
+                clearTimeout(runNextAuto);
+                runNextAuto = setTimeout(() => {
+                    next.click();
+                }, timeAutoNext)
+            }
